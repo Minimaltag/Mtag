37ec1edef791a4730f655621b812735a18ebd005
Minimaltag <minimaltag7@gmail.com>
2025-06-09 18:26:37 -0400

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCnPcxbJaG3kwBjU7QRmTwOYM6amuAjx1o",
+  authDomain: "teste-eff5e.firebaseapp.com",
+  projectId: "teste-eff5e",
+  storageBucket: "teste-eff5e.firebasestorage.app",
+  messagingSenderId: "272404465161",
+  appId: "1:272404465161:web:260b0242408081a17c9ad8"
+};
